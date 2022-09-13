181b053a0eba03d43cdc46a80fd4b25290c3ebbc
ameshchanuka <ameshchanuka@gmail.com>
2022-09-14 00:05:50 +0530

    First upload of the backend

---
+++ controllers/workoutController.js
@@ -0,0 +1,55 @@
+const { default: mongoose } = require('mongoose')
+const Workout = require('../models/workoutModel')
+
+// Get all workouts
+const getWorkouts = async (req, res) => {
+    const workout = await Workout.find({}).sort({ createdAt: -1 })
+
+    res.status(200).json(workout)
+}
+
+
+// get a single workout
+const getWorkout = async (req, res) => {
+    const { id } = req.params
+
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+        return res.status(404).json({ error: 'No such workout' })
+    }
+
+    const workout = await Workout.findById(id)
+
+    if (!workout) {
+        return res.status(400).json({ error: 'No such workout' })
+    }
+    res.status(200).json(workout)
+}
+
+
+// create new workout
+const createWorkout = async (req, res) => {
+    const { title, load, reps } = req.body
+
+    // add a doc to db
+    try {
+        const workout = await Workout.create({ title, load, reps })
+        res.status(200).json(workout)
+    }
+    catch (error) {
+        res.status(400).json({ error: error.message })
+    }
+}
+
+
+// delete a workout
+
+
+
+// update a workout
+
+
+module.exports = {
+    getWorkouts,
+    getWorkout,
+    createWorkout
+}
